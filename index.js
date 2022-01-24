ac8835aa7aeab48046465d83d111693bd4143479
Martin Mwangi <mwangimartin1904@gmail.com>
2022-01-24 13:18:32 +0300

    Setup security

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const DbConnect = require("./utils/dbConnect");
+DbConnect();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/static", express.static("uploads"));
+// set security http headers
+app.use(helmet());
 
-DbConnect();
+//
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use("/static", express.static(__dirname + "/public"));
+
+//  set limit request from same API in timePeroid from same ip
+const limiter = rateLimit({
+  max: 1000, //   max number of limits
+  windowMs: 60 * 60 * 1000, // hour
+  message: " Too many req from this IP , please Try  again in an Hour ! ",
+});
+
+app.use("/api", limiter);
+
+//  Body Parser  => reading data from body into req.body protect from scraping etc
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSql query injection
+app.use(mongoSanitize()); //   filter out the dollar signs protect from  query injection attact
+
+// Data sanitization against XSS
+app.use(xss()); //    protect from molision code coming from html
 
 app.get("/", (req, res) => {
   res.send("App running....");
 });
 
+// handling all (get,post,update,delete.....) unhandled routes
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
+
+// Start server
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
 
