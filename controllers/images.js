fa84e89df38ae1026d8c7b8db0561d7ee383f5f6
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-07 18:53:10 +0300

    :sparkles: handle delete image

--- controllers/images.js
+++ controllers/images.js
@@ -3,44 +3,66 @@ const crypto = require("crypto");
 const Image = require("../models/Image");
 
 module.exports = {
-  uploadImage: catchAsync(async (req, res) => {
-    if (!req.files || !req.files.image) {
-      return res
-        .status("400")
-        .send({ success: false, message: "No 'image' selected" });
-    }
+    uploadImage: catchAsync(async (req, res) => {
+        if (!req.files || !req.files.image) {
+            return res
+                .status("400")
+                .send({ success: false, message: "No 'image' selected" });
+        }
 
-    const id = crypto.randomBytes(16).toString("hex");
+        const id = crypto.randomBytes(16).toString("hex");
 
-    const { image } = req.files;
-    const { title, date, description } = req.body;
+        const { image } = req.files;
+        const { title, date, description } = req.body;
 
-    const imageLink = `${id +"_"+ image.name}`;
-    image.mv(`uploads/${imageLink}`);
+        const imageLink = `${id + "_" + image.name}`;
+        image.mv(`uploads/${imageLink}`);
 
-    const uploadImage = await Image.create({
-      title,
-      description,
-      date: new Date(date),
-      image: imageLink,
-    });
+        const uploadImage = await Image.create({
+            title,
+            description,
+            date: new Date(date),
+            image: imageLink,
+        });
 
-    res.send({ success: true, message: "Image updated successfully.", image: uploadImage });
-  }),
-  getImages: catchAsync(async (req, res) => {
-    const pagesize = req.query.pagesize || 10;
-    const page = req.query.page || 1;
+        res.send({
+            success: true,
+            message: "Image updated successfully.",
+            image: uploadImage,
+        });
+    }),
 
-    const query = {
-      skip: pagesize * (page > 0 ? page - 1 : 1),
-      limit: pagesize,
-    };
+    getImages: catchAsync(async (req, res) => {
+        const pagesize = req.query.pagesize || 10;
+        const page = req.query.page || 1;
 
-    const allImages = await Image.find();
-    const total = allImages.length;
+        const query = {
+            skip: pagesize * (page > 0 ? page - 1 : 1),
+            limit: pagesize,
+        };
 
-    const images = await Image.find({}, {}, query).select("-__v");
+        const allImages = await Image.find();
+        const total = allImages.length;
 
-    res.send({ success: true, images, total });
-  }),
+        const images = await Image.find({}, {}, query).select("-__v");
+
+        res.send({ success: true, images, total });
+    }),
+
+    deleteImageController: catchAsync(async (req, res) => {
+        const { id } = req.params;
+
+        const image = await Image.findById(id);
+        if (!image)
+            return res
+                .status(404)
+                .send({ success: false, message: "Image not found" });
+
+        await Image.findByIdAndDelete(id);
+
+        res.status(200).json({
+            success: true,
+            message: `Deleted Successfull.`,
+        });
+    }),
 };
--- routes/images.js
+++ routes/images.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { uploadImage, getImages } = require("../controllers/images");
+const { uploadImage, getImages, deleteImageController } = require("../controllers/images");
 
 const schemaValidator = require("../middlewares/schemaValidator");
 const { uploadImageSchema } = require("../schemas/image");
@@ -8,4 +8,5 @@ const auth = require("../middlewares/auth");
 
 router.post("/", auth, schemaValidator(uploadImageSchema, "body"), uploadImage);
 router.get("/",  getImages);
+router.delete("/:id",  deleteImageController);
 module.exports = router;
