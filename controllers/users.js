c05cde7866faf4d362a0051ec263dd240810d37f
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-10 09:44:14 +0300

    :sparkles: handle search user

--- controllers/users.js
+++ controllers/users.js
@@ -83,21 +83,16 @@ module.exports = {
     }),
 
     removeAdminController: catchAsync(async (req, res) => {
-        const { email } = req.body;
-        if (!email)
-            return res
-                .status(400)
-                .send({ success: false, message: "email is required" });
+        const { id } = req.params;
 
-        let user = await User.findOne({ email }).select("+password"); // select expiclity password
-
+        const user = await User.findById(id);
         if (!user)
             return res
                 .status(404)
-                .send({ success: false, message: "Email does not exist" });
+                .send({ success: false, message: "Account does not exist" });
 
-        user = await User.findByIdAndUpdate(
-            user._id,
+        await User.findByIdAndUpdate(
+            id,
             {
                 $set: {
                     role: "user",
@@ -149,18 +144,79 @@ module.exports = {
                 .send({ success: false, message: "Account does not exist" });
 
         if (user.role === "admin")
-            return res
-                .status(404)
-                .send({
-                    success: false,
-                    message: "Please remove user from admin list first",
-                });
+            return res.status(400).send({
+                success: false,
+                message: "Please remove user from admin list first",
+            });
 
         await User.findByIdAndDelete(id);
 
         res.status(200).json({
             success: true,
             message: `Deleted Successfull.`,
+        });
+    }),
+
+    searchUserController: catchAsync(async (req, res) => {
+        const search = req.query.search || "";
+
+        const pagesize = req.query.pagesize || 5;
+        const page = req.query.page || 1;
+
+        const query = {
+            skip: pagesize * (page > 0 ? page - 1 : 1),
+            limit: pagesize,
+        };
+
+        if (search === "")
+            return res.status(200).json({
+                success: true,
+                message: `Successfull.`,
+                results: [],
+            });
+
+        const rgx = (pattern) => new RegExp(`.*${pattern}.*`);
+        const searchRgx = rgx(search);
+
+        const users = await User.find(
+            {
+                $or: [
+                    {
+                        firstname: { $regex: searchRgx, $options: "i" },
+                    },
+                    {
+                        lastname: { $regex: searchRgx, $options: "i" },
+                    },
+                    {
+                        email: { $regex: searchRgx, $options: "i" },
+                    },
+                ],
+            },
+            {},
+            query
+        )
+            .sort([["createdAt", -1]])
+            .select("-password");
+
+        const allUsers = await User.find({
+            $or: [
+                {
+                    firstname: { $regex: searchRgx, $options: "i" },
+                },
+                {
+                    lastname: { $regex: searchRgx, $options: "i" },
+                },
+                {
+                    email: { $regex: searchRgx, $options: "i" },
+                },
+            ],
+        });
+        const total = allUsers.length;
+
+        res.status(200).json({
+            success: true,
+            users,
+            total,
         });
     }),
 };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { uploadAvatar, getUsers, createAdmin, getAdmins, newsletterSubscription, removeAdminController, deleteUserController } = require("../controllers/users");
+const { uploadAvatar, getUsers, createAdmin, getAdmins, newsletterSubscription, removeAdminController, deleteUserController, searchUserController } = require("../controllers/users");
 
 const auth = require("../middlewares/auth");
 
@@ -9,8 +9,9 @@ const { subscribeEmailSchema } = require("../schemas/user");
 router.get("/", auth, getUsers);
 router.put("/upload-avatar/:id", auth, uploadAvatar);
 router.post("/admin", auth, createAdmin);
-router.post("/admin/remove", auth, removeAdminController);
+router.delete("/admin/:id", auth, removeAdminController);
 router.get("/admin", auth, getAdmins);
 router.delete("/:id", auth, deleteUserController);
 router.post("/email/subscribe", schemaValidator(subscribeEmailSchema, "body"), newsletterSubscription);
+router.get("/search", auth, searchUserController);
 module.exports = router;
