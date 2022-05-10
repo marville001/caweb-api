f304e73b9db21f2d0463534d3c6774df277f657f
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-10 09:07:32 +0300

    :sparkles: remove user and admin

--- controllers/users.js
+++ controllers/users.js
@@ -32,6 +32,7 @@ module.exports = {
         );
         res.send({ success: true, message: "Profile picture updated.", user });
     }),
+
     getUsers: catchAsync(async (req, res) => {
         const pagesize = req.query.pagesize || 5;
         const page = req.query.page || 1;
@@ -44,7 +45,9 @@ module.exports = {
         const allUsers = await User.find();
         const total = allUsers.length;
 
-        const users = await User.find({}, {}, query).sort([["createdAt", -1]]).select("-password");
+        const users = await User.find({}, {}, query)
+            .sort([["createdAt", -1]])
+            .select("-password");
 
         res.send({ success: true, users, total });
     }),
@@ -78,8 +81,45 @@ module.exports = {
 
         res.send({ success: true, user, message: "User updated to admin!" });
     }),
+
+    removeAdminController: catchAsync(async (req, res) => {
+        const { email } = req.body;
+        if (!email)
+            return res
+                .status(400)
+                .send({ success: false, message: "email is required" });
+
+        let user = await User.findOne({ email }).select("+password"); // select expiclity password
+
+        if (!user)
+            return res
+                .status(404)
+                .send({ success: false, message: "Email does not exist" });
+
+        user = await User.findByIdAndUpdate(
+            user._id,
+            {
+                $set: {
+                    role: "user",
+                },
+            },
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        res.send({
+            success: true,
+            user,
+            message: "User updated to normal user!",
+        });
+    }),
+
     getAdmins: catchAsync(async (req, res) => {
-        const admins = await User.find({ role: "admin" }).sort([["createdAt", -1]]).select("-password");
+        const admins = await User.find({ role: "admin" })
+            .sort([["createdAt", -1]])
+            .select("-password");
 
         res.send({ success: true, admins });
     }),
@@ -97,5 +137,30 @@ module.exports = {
         newsletterUser.save({ validateBeforeSave: false });
 
         res.send({ success: true, message: "Subscription successfull" });
+    }),
+
+    deleteUserController: catchAsync(async (req, res) => {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+        if (!user)
+            return res
+                .status(404)
+                .send({ success: false, message: "Account does not exist" });
+
+        if (user.role === "admin")
+            return res
+                .status(404)
+                .send({
+                    success: false,
+                    message: "Please remove user from admin list first",
+                });
+
+        await User.findByIdAndDelete(id);
+
+        res.status(200).json({
+            success: true,
+            message: `Deleted Successfull.`,
+        });
     }),
 };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { uploadAvatar, getUsers, createAdmin, getAdmins, newsletterSubscription } = require("../controllers/users");
+const { uploadAvatar, getUsers, createAdmin, getAdmins, newsletterSubscription, removeAdminController, deleteUserController } = require("../controllers/users");
 
 const auth = require("../middlewares/auth");
 
@@ -9,6 +9,8 @@ const { subscribeEmailSchema } = require("../schemas/user");
 router.get("/", auth, getUsers);
 router.put("/upload-avatar/:id", auth, uploadAvatar);
 router.post("/admin", auth, createAdmin);
+router.post("/admin/remove", auth, removeAdminController);
 router.get("/admin", auth, getAdmins);
+router.delete("/:id", auth, deleteUserController);
 router.post("/email/subscribe", schemaValidator(subscribeEmailSchema, "body"), newsletterSubscription);
 module.exports = router;
