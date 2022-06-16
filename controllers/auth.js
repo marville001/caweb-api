4d35919cf1f1f47ce07d31bd84069c804db88f8f
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-16 11:47:38 +0300

    Updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const _ = require("lodash");
 const catchAsync = require("../utils/catchAsync");
 const sendEmail = require("../utils/sendEmail");
