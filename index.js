c2863d470421bf47e406ae459568b6de35db0082
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-21 08:59:12 +0300

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
-var path = require('path');
+var path = require("path");
 
 // App routes
 const authRoutes = require("./routes/auth");
@@ -24,11 +24,14 @@ DbConnect();
 
 const app = express();
 
-var dir = path.join(__dirname, 'uploads');
+var dir = path.join(__dirname, "uploads");
 
 app.use("/static", express.static(dir));
 
-app.use(cors());
+const options = {
+    origin: ["http://localhost:3000", "https://dekutcatholicchaplaincy.org"],
+};
+app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -45,7 +48,7 @@ app.use(mongoSanitize()); //   filter out the dollar signs protect from  query i
 app.use(xss()); //    protect from molision code coming from html
 
 app.get("/", (req, res) => {
-  res.send("App running....");
+    res.send("App running....");
 });
 
 app.use("/api/auth", authRoutes);
@@ -61,7 +64,7 @@ app.use("/api/contact", contactRoutes);
 
 // handling all (get,post,update,delete.....) unhandled routes
 app.all("*", (req, res, next) => {
-  res.status(404).send(`Can't find ${req.originalUrl} on the server`);
+    res.status(404).send(`Can't find ${req.originalUrl} on the server`);
 });
 
 // Start server
@@ -71,13 +74,13 @@ app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
 // handle Globaly  the unhandle Rejection Error which is  outside the express
 // e.g database connection
 process.on("unhandledRejection", (error) => {
-  // it uses unhandledRejection event
-  // using unhandledRejection event
-  console.log(" Unhandled Rejection => shutting down..... ");
-  console.log(error.name, error.message);
-  app.close(() => {
-    process.exit(1); //  emediatly exists all from all the requests sending OR pending
-  });
+    // it uses unhandledRejection event
+    // using unhandledRejection event
+    console.log(" Unhandled Rejection => shutting down..... ");
+    console.log(error.name, error.message);
+    app.close(() => {
+        process.exit(1); //  emediatly exists all from all the requests sending OR pending
+    });
 });
 
 module.exports = app;
