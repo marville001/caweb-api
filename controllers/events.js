579684ca04b26ccbd5e65e597b7db81898c0ff73
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-29 22:10:16 +0300

    migrate events operations

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,15 @@
+const { sequelize } = require("../models");
 const catchAsync = require("../utils/catchAsync");
+const { Op } = require("sequelize");
 
 module.exports = {
     addEventController: catchAsync(async (req, res) => {
+        const event = await sequelize.models.events.create(req.body);
+
         res.send({
             success: true,
             message: "Event Added successfully.",
-            event: {},
+            event,
         });
     }),
 
@@ -13,23 +17,92 @@ module.exports = {
         const search = req.query.search || "";
         const pagesize = req.query.pagesize || 10;
         const page = req.query.page || 1;
+        const order = req.query.order || "ASC";
+        const sortby = req.query.sortby || "id";
 
-        res.send({ success: true, events: [], total: 0 });
+        let where = {};
+
+        if (search !== "") {
+            where = {
+                [Op.or]: [
+                    {
+                        title: {
+                            [Op.iLike]: `%${search}%`,
+                        },
+                    },
+                    {
+                        description: {
+                            [Op.iLike]: `%${search}%`,
+                        },
+                    },
+                ],
+            };
+        }
+
+        const limit = pagesize <= 0 ? null : pagesize;
+        const offset =
+            page <= 0 ? 0 : (page - 1) * (pagesize <= 0 ? 0 : pagesize);
+
+        const total = await sequelize.models.events.count({ where });
+
+        let events = await sequelize.models.events.findAll({
+            where,
+            limit,
+            offset,
+            order: [[sortby, order]],
+            subQuery: false,
+            // include: sequelize.models.users,
+        });
+
+        res.send({ success: true, events, total });
     }),
 
     getEventController: catchAsync(async (req, res) => {
-        res.send({ success: true, event: {} });
+        const { key } = req.params;
+
+        const event = await sequelize.models.events.findOne({ where: { key } });
+
+        if (!event)
+            return res
+                .status(404)
+                .send({ success: false, message: "Event not found" });
+
+        res.send({ success: true, event });
     }),
 
     updateEventController: catchAsync(async (req, res) => {
+        const { id } = req.params;
+
+        let event = await sequelize.models.events.findByPk(id);
+        if (!event)
+            return res
+                .status(404)
+                .send({ success: false, message: "Event does not exist" });
+
+        await sequelize.models.events.update(req.body, {
+            where: { id },
+        });
+
+        event = await sequelize.models.events.findByPk(id);
+
         res.send({
             success: true,
             message: "Updated successfully!",
-            event: {},
+            event,
         });
     }),
 
     deleteEventController: catchAsync(async (req, res) => {
+        const { id } = req.params;
+
+        let event = await sequelize.models.events.findByPk(id);
+        if (!event)
+            return res
+                .status(404)
+                .send({ success: false, message: "Event does not exist" });
+
+        await sequelize.models.events.destroy({ where: { id } });
+        
         res.status(200).json({
             success: true,
             message: `Deleted Successfull.`,
